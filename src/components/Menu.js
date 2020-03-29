7162fc18fd054a64a17d03ed75111a2123d95b32
Lucas Olmos <lucolmos@gmail.com>
2020-03-28 21:47:53 -0300

    modif

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,18 +3,22 @@ import { Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Menu(props) {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Home</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Datos Personales</Nav.Link>
-        <Nav.Link href="#education">Educacion</Nav.Link>
-        <Nav.Link href="#workexperience">Experiencia Laboral</Nav.Link>
-        <Nav.Link href="#qualifications">Cualificaciones</Nav.Link>
-        <NavDropdown title="Descargar" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#download">cuurriculum.pdf</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar>
+    <header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Datos Personales</Nav.Link>
+          <Nav.Link href="#education">Educacion</Nav.Link>
+          <Nav.Link href="#workexperience">Experiencia Laboral</Nav.Link>
+          <Nav.Link href="#qualifications">Cualificaciones</Nav.Link>
+          <NavDropdown title="Descargar" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#download">
+              cuurriculum.pdf
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar>
+    </header>
   );
 }
 
