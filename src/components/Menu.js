29000928520e0b1b12aa5eaf99bb721271502f5e
Lucas Olmos <lucolmos@gmail.com>
2020-03-29 18:35:06 -0300

    change styles

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,50 @@
 import React from "react";
-import { Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import logoImg from "../assets/img/LO(1).png";
+import {
+  Button,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Image
+} from "react-bootstrap";
 
 function Menu(props) {
   return (
-    <header>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Home</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+        <Image
+          src={logoImg}
+          width="35"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+          roundedCircle
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
           <Nav.Link href="#home">Datos Personales</Nav.Link>
           <Nav.Link href="#education">Educacion</Nav.Link>
           <Nav.Link href="#workexperience">Experiencia Laboral</Nav.Link>
-          <Nav.Link href="#qualifications">Cualificaciones</Nav.Link>
-          <NavDropdown title="Descargar" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#download">
-              cuurriculum.pdf
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#qualifications">Skills</Nav.Link>
         </Nav>
-      </Navbar>
-    </header>
+        <NavDropdown
+          className="navbar-nav drop1"
+          title="Descargar"
+          id="basic-nav-dropdown"
+        >
+          <NavDropdown.Item href="#action/3.1">CV.pdf</NavDropdown.Item>
+        </NavDropdown>
+        {/* <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form> */}
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
