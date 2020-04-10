a8f0f70969e9918da502d30a90cf93a981d80ccb
Lucas Olmos <lucolmos@gmail.com>
2020-04-10 16:55:00 -0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,20 @@ import React from "react";
 function Footer(props) {
   return (
     <footer className="footer">
-      <div className="divFooter">
-        <a
-          href="https://www.linkedin.com/in/lucasolmos44"
-          target="_blank"
-          title="as"
-        >
-          <ion-icon class="icons" name="logo-linkedin" size="large"></ion-icon>
-        </a>
-        <a href="https://github.com/lucasolmos04" target="_blank" title="as">
-          <ion-icon class="icons" name="logo-github" size="large"></ion-icon>
-        </a>
-      </div>
+      <a
+        href="https://www.linkedin.com/in/lucasolmos44"
+        target="_blank"
+        title="as"
+      >
+        <ion-icon class="icons" name="logo-linkedin" size="large"></ion-icon>
+      </a>
+      <a href="https://github.com/lucasolmos04" target="_blank" title="as">
+        <ion-icon class="icons" name="logo-github" size="large"></ion-icon>
+      </a>
+      <br></br>
+      <span className="copyright">
+        Copyright (c) {new Date().getFullYear()} Lucas Maximiliano Olmos
+      </span>
     </footer>
   );
 }
