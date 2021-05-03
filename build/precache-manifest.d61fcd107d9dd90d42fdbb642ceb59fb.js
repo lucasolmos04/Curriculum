6c7eaf01f6ac8bac83480929994723ecca48a9d3
Lucas Olmos <lucolmos@gmail.com>
2021-05-02 22:18:39 -0300

    add folder build

---
+++ build/precache-manifest.d61fcd107d9dd90d42fdbb642ceb59fb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e75b1e4ca3af236d7f8f511d58b3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa15810390a53e2b6119",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ce6474d1ab2bfc819564",
+    "url": "/static/css/main.05d2988b.chunk.css"
+  },
+  {
+    "revision": "aa15810390a53e2b6119",
+    "url": "/static/js/2.c3d4b548.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.c3d4b548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6474d1ab2bfc819564",
+    "url": "/static/js/main.39593935.chunk.js"
+  },
+  {
+    "revision": "7c752006a8cb1ff38264",
+    "url": "/static/js/runtime-main.8d7c8459.js"
+  },
+  {
+    "revision": "dde62d62fde1ec13b058fcef9ad558a6",
+    "url": "/static/media/CV_LucasOlmos_DevSoftware.dde62d62.pdf"
+  },
+  {
+    "revision": "4bb15859f98ea927a766f5c9ce145cb5",
+    "url": "/static/media/UTN_logo.4bb15859.ico"
+  },
+  {
+    "revision": "4eee67f4d3f5232b44b8001df4262cad",
+    "url": "/static/media/aulica.4eee67f4.ico"
+  },
+  {
+    "revision": "45112be85cde419799b88514299c223d",
+    "url": "/static/media/background2.45112be8.png"
+  },
+  {
+    "revision": "c5ebc54778c8cdf20f642f9b5ee91329",
+    "url": "/static/media/bancor1.c5ebc547.ico"
+  },
+  {
+    "revision": "265f99d9368cd127aeb2fc275a086064",
+    "url": "/static/media/dicsys-.265f99d9.ico"
+  },
+  {
+    "revision": "7fc091e2906c46260663ddd2c76f15f3",
+    "url": "/static/media/foto5_Cara.7fc091e2.jpg"
+  },
+  {
+    "revision": "459fa0f1a9ef6553d7d74d65d184b430",
+    "url": "/static/media/incluit.459fa0f1.ico"
+  },
+  {
+    "revision": "c08a72721171a1b28b9470d0b80ddb41",
+    "url": "/static/media/udemy.c08a7272.ico"
+  }
+]);
